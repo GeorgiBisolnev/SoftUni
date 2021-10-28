f2665e1067d345879af89b55922712a27ee44640
GeorgiBisolnev <93096138+GeorgiBisolnev@users.noreply.github.com>
2021-10-28 21:21:16 +0300

    update 6

---
+++ OOP/Exercise Inheritance/Animals/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+
+    public class Animal
+    {
+        private string name;
+        private string gender;
+        private int age;
+        public string Name { get=>name; set 
+                {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                
+                name = value;
+            }
+        }
+        public int  Age { get=>age; set {
+                if (value<0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                age = value;
+            } }
+        public String Gender { get=>gender; set {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                {
+                    throw new ArgumentException("Invalid input!"); ;
+                }
+                gender = value;
+            } }
+
+        public Animal(string name, int age, String gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public virtual string ProduceSound()
+        {
+            return "n/a";
+        }
+    }
+}
--- OOP/Exercise Inheritance/Animals/StartUp.cs
+++ OOP/Exercise Inheritance/Animals/StartUp.cs
@@ -1,10 +1,70 @@
-﻿namespace Animals
+﻿using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            //Animals asd = new Kitten("Asd", 13);
+            //asd.ProduceSound();
+            List<Animal> animals = new List<Animal>();
+            string input = System.Console.ReadLine();
+            string gender = "n/a";
+                int age = 0;
+            string name = "n/a";
+            string[] info = null;
+            while (input != "Beast!")
+            {
+                switch (input)
+                {
+                    case "Dog":
+                        info = System.Console.ReadLine().Split();
+                        name = info[0];
+                        age = int.Parse(info[1]);
+                        gender = info[2];
+                        animals.Add(new Dog(name, age, gender));
+                        break;
+                    case "Cat":
+                        info = System.Console.ReadLine().Split();
+                        name = info[0];
+                        age = int.Parse(info[1]);
+                        gender = info[2];
+                        animals.Add(new Cat(name, age, gender));
+                        break;
+                    case "Frog":
+                        info = System.Console.ReadLine().Split();
+                        name = info[0];
+                        age = int.Parse(info[1]);
+                        gender = info[2];
+                        animals.Add(new Frog(name, age, gender));
+                        break;
+                    case "Kitten":
+                        info = System.Console.ReadLine().Split();
+                        name = info[0];
+                        age = int.Parse(info[1]);
+                        //gender = info[2];
+                        animals.Add(new Kitten(name, age));
+                        break;
+                    case "Tomcat":
+                        info = System.Console.ReadLine().Split();
+                        name = info[0];
+                        age = int.Parse(info[1]);
+                        //gender = info[2];
+                        animals.Add(new Tomcat(name, age));
+                        break;
+                    default:
+                        break;
+                }
+                input = System.Console.ReadLine();
+            }
+
+            foreach (Animal animal in animals)
+            {
+                System.Console.WriteLine(animal.GetType().Name);
+                System.Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                System.Console.WriteLine(animal.ProduceSound()); 
+            }
         }
     }
 }
