9aed46e3edbbba259230a25bb418a130e34e6740
GeorgiBisolnev <93096138+GeorgiBisolnev@users.noreply.github.com>
2021-11-06 13:12:17 +0200

    .

---
+++ OOP/Interfaces and Abstraction/Border Control/BorderControl/BorderControl/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IPerson , IId 
+    {
+        public Citizen(string name, int age, string id)
+        {
+            Name = name;
+            Age = age;
+            this.id = id;
+        }
+
+        public string Name { get;set; }
+        public int Age { get; set; }
+        public string id { get; set; }
+
+    }
+}
--- OOP/Interfaces and Abstraction/Border Control/BorderControl/BorderControl/Program.cs
+++ OOP/Interfaces and Abstraction/Border Control/BorderControl/BorderControl/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BorderControl
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IId> ids = new List<IId>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+
+                string[] info = command.Split();
+                if (info.Length>2)
+                {
+                    ids.Add(new Citizen(info[0], int.Parse(info[1]), info[2]));
+                }else
+                {
+                    ids.Add(new Robot(info[0], info[1]));
+                }
+            }
+
+            string delete = (Console.ReadLine());
+
+            foreach (var humanOrRobot in ids)
+            {
+                if (humanOrRobot.id.Length> delete.Length && humanOrRobot.id.Substring(humanOrRobot.id.Length- delete.Length, delete.Length) == delete && delete!="")
+                {
+                    Console.WriteLine(humanOrRobot.id);
+                }
+            }
         }
     }
 }
