0065a08b0df0335798bc7f86cd0f30e8644606ab
GeorgiBisolnev <93096138+GeorgiBisolnev@users.noreply.github.com>
2022-07-24 22:19:42 +0300

    Problem 001 Solved

---
+++ C# DB/JSON Processing/08. JSON-Processing-Product-Shop-Skeleton OLD/ProductShop/DTOs/Users/ImportUserDto.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DTOs.Users
+{
+    [JsonObject]
+    public class ImportUserDto
+    {
+        [JsonProperty("firstName")]
+        public string  FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string  LastName { get; set; }
+
+        [JsonProperty("age")]
+        public int? Age { get; set; }
+    }
+}
---
+++ C# DB/JSON Processing/08. JSON-Processing-Product-Shop-Skeleton OLD/ProductShop/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Users;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static IMapper mapper;
+        public static void Main(string[] args)
+        {
+            mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+
+            ProductShopContext context = new ProductShopContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            //Console.WriteLine("Let's go ...");
+            string jsonInput = File.ReadAllText("../../../Datasets/users.json");
+
+            Console.WriteLine(ImportUsers(context, jsonInput));
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var str = new StringBuilder();
+
+            ImportUserDto[] userDTO = JsonConvert
+                .DeserializeObject<ImportUserDto[]>(inputJson);
+
+            ICollection<User> users = new List<User>();
+
+            foreach (ImportUserDto uDto in userDTO)
+            {
+                User user = mapper.Map<User>(uDto);
+
+                users.Add(user);
+            }
+
+            context.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
+        }
+    }
+    
+}
